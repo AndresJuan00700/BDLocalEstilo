584c21dc5b3af48b7ef58b95feb532aee8b16aa2
57300 <57300@DESKTOP-73CBDRP>
2022-04-25 07:51:19 -0500

    Agregar archivos de proyecto.

---
+++ BDLocal2/BDLocal2/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using BDLocal2.BD;
+using BDLocal2.Dependecy;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BDLocal2
+{
+    public partial class App : Application
+    {
+
+        private static PersonaDataBase baseDatos;
+
+        public static PersonaDataBase BaseDatos
+        {
+            get
+            {
+                if (baseDatos == null)
+                {
+                    return baseDatos = new PersonaDataBase(DependencyService.Get<IRuta>().GetPathBb("PersonaDB.bd3"));
+                }
+                else
+                {
+                    return baseDatos;
+                }
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
